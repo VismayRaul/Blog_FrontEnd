17ddf98367db3c7c6178c33d4ab48122139cea3c
Zain <>
2022-09-17 13:25:33 +0530

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 class="text-3xl font-bold underline py-10">
-          Hello world!
+          Hello world!!!!!!
         </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
